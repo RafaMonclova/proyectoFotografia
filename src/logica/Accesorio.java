f52d3b51fd88a11baa7cc43bd2da2445015aaf26
alumno <alumno@pc02-t1.inf>
2022-05-13 18:14:54 +0200

    Inicio

---
+++ src/logica/Accesorio.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author RAFAEL MONCLOVA SUANO
+ */
+public class Accesorio {
+    
+    private int id;
+    private String marca;
+    private String nombre;
+    private double precio;
+
+    public Accesorio(int id, String marca, String nombre, double precio) {
+        this.id = id;
+        this.marca = marca;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
---
+++ src/logica/Camara.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author RAFAEL MONCLOVA SUANO
+ */
+public class Camara {
+    
+    private int id;
+    private String marca;
+    private String modelo;
+    private double precio;
+    private ArrayList<Accesorio> accesorios = new ArrayList();
+
+    public Camara(int id,String marca, String modelo, double precio) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<Accesorio> getAccesorios() {
+        return accesorios;
+    }
+
+    public void setAccesorios(ArrayList<Accesorio> accesorios) {
+        this.accesorios = accesorios;
+    }
+    
+    public void añadirAccesorio(Accesorio a){
+        
+        accesorios.add(a);
+        
+    }
+    
+    public void eliminarAccesorio(Accesorio a){
+        
+        Iterator it = accesorios.iterator();
+        
+        while(it.hasNext()){
+            Accesorio actual  = (Accesorio) it.next();
+            if(actual.getId() == a.getId()){
+                    it.remove();
+            }
+        }
+        
+    }
+    
+    
+    
+}
